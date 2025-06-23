cb843a3451ebe13a1041a7cb042628b6e3aa3542
Ojed AI <ojedaiecode@gmail.com>
2025-06-23 16:23:58 -0300

    fix: Implementa configuração CORS conforme especificado pelo General - Remove handler manual OPTIONS - Configura CORS com headers e métodos corretos

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ Este módulo é responsável por:
 4. Inicializar serviços externos
 """
 
-from flask import Flask, request, make_response
+from flask import Flask
 from flask_cors import CORS
 from dotenv import load_dotenv
 import os
@@ -28,22 +28,14 @@ def create_app():
     app = Flask(__name__)
     
     # Configuração do CORS conforme especificado pelo General
-    CORS(
-        app, 
-        resources={r"/*": {"origins": "https://tvplaydastorcidas.com"}}, 
-        supports_credentials=True
+    CORS(app,
+        resources={r"/*": {
+            "origins": ["https://tvplaydastorcidas.com"],
+            "methods": ["GET", "POST", "OPTIONS"],
+            "allow_headers": ["Content-Type", "Authorization"],
+            "supports_credentials": True
+        }}
     )
-    
-    # Handler manual para requisições OPTIONS
-    @app.before_request
-    def handle_options():
-        if request.method == 'OPTIONS':
-            response = make_response()
-            response.headers['Access-Control-Allow-Origin'] = 'https://tvplaydastorcidas.com'
-            response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
-            response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
-            response.status_code = 200
-            return response
     
     # Configurações da aplicação
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
